29eb013b6e658845545849afbad9ddf2a9d915de
Sandra Calero <3358@holbertonschool.com>
2021-08-14 11:29:19 -0500

    add header file, strlen function and prompt function

---
+++ holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <unistd.h>
+
+int _strlen(char *s);
+
+#endif /* HOLBERTON_H */
---
+++ prompt.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * main - prints prompt and wait for a command
+ *
+ * Return: 0 success.
+ */
+int main(void)
+{
+	char *prompt = "$ ";
+	size_t len_prompt = _strlen(prompt);
+	char *line = NULL;
+	size_t len_line = 0;
+	ssize_t num_chars_line = 0;
+
+	while (1)
+	{
+		write(STDOUT_FILENO, prompt, len_prompt);
+		num_chars_line = getline(&line, &len_line, stdin);
+		if (num_chars_line < 0)
+		{
+			if (errno)
+			{
+				perror("This command is not correct");
+			}
+			exit(1);
+		}
+		write(STDOUT_FILENO, line, num_chars_line);
+	}
+	return (0);
+}
