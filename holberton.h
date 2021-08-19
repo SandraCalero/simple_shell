9f47c18f8126c8f2b76c49bf4c77e22317ac0f4e
Juanse1595 <3309@holbertonschool.com>
2021-08-19 22:05:36 +0000

    CHANGE: Adding _strcmp to header

--- holberton.h
+++ holberton.h
@@ -24,6 +24,7 @@ void _env(char **env);
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
 int _strncmp(char *s1, char *s2, size_t len);
 
 #endif /* HOLBERTON_H */
