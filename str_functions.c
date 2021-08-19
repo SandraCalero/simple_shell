28e7383894e9455350b13350c14afb46a0060a49
Juanse1595 <3309@holbertonschool.com>
2021-08-19 22:07:37 +0000

    CHANGE: Adding _strcmp function

--- str_functions.c
+++ str_functions.c
@@ -78,3 +78,21 @@ int _strncmp(char *s1, char *s2, size_t len)
 	}
 	return (rest);
 }
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first sitrng
+ * @s2: second string
+ * Return: an int greater, equal or less than 0 depending
+ * on the comparison
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while ((s1[i] - s2[i]) == 0 && s1[i] && s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
